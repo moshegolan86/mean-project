50cafce3fac20e515024794dfc237ec8cff8590c
Moshe Golan <moshegolan86@gmail.com>
2019-04-04 14:06:42 +0300

    adding user and admin properties in backend/models

---
+++ backend/models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+
+var user = mongoose.Schema({
+  email: { type: String, required: true},
+  password: { type: String, required: true, select: false},
+  firstName: { type: string, required: true},
+  lastName: { type: string, required: true},
+  phoneNumber: { type: phoneNumber, required: true},
+  isAdmin: { type: boolean, required: false}
+
+});
+
+module.exports = mongoose.model('User', user);
+
